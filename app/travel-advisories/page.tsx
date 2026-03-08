0f99f9a83ddd84712677565b6b874e2da7e242ff
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/travel-advisories/page.tsx
+++ app/travel-advisories/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import TravelAdvisoryCard from '../components/TravelAdvisoryCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Travel Advisories | Embassy',
